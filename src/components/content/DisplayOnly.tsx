8139af07d353e1cd31c39166a993f848047a8913
IAN.C <ian.chen@sjfood.com>
2024-01-27 11:24:20 +0800

    代碼優化

--- src/components/content/DisplayOnly.tsx
+++ src/components/content/DisplayOnly.tsx
@@ -11,10 +11,10 @@ export interface DisplayOnlyProps {
 export default function DisplayOnly(props: DisplayOnlyProps) {
   const content = useMemo(() => {
     let content;
-    if (typeof(props.value) == 'string') {
+    if (typeof (props.value) == 'string') {
       const value = props.value.trim();
       if (value.startsWith('<')) { // 富文本
-        content = <div dangerouslySetInnerHTML={{__html: props.value}} />;
+        content = <div dangerouslySetInnerHTML={{ __html: props.value }} />;
       } else if (props.componentProps?.options) { // Select（單選）或 Radio
         content = props.componentProps.options.find(value).label;
       } else if (props.componentProps?.treeData) { // TreeSelect（單選）
@@ -25,14 +25,14 @@ export default function DisplayOnly(props: DisplayOnlyProps) {
         content = value;
       }
     } else if (props.componentProps != undefined) {
-      const values = typeof(props.value) == 'object' ? (props.value as AnyObject) : [props.value];
+      const values = typeof (props.value) == 'object' ? (props.value as AnyObject) : [props.value];
       if (props.componentProps?.options) { // Select（多選）或 CheckBox 或 Radio
         content = props.componentProps.options.filter((item: AnyObject) => {
           return values.includes(item.value);
         }).map((item: AnyObject) => item.label).join('、');
       } else if (props.componentProps?.treeData) { // TreeSelect（多選）
         content = recursiveMap(props.componentProps.treeData, (item: AnyObject) => {
-          return values.includes(item.value) ? item.title : false;
+          return values.includes(item.value || item.key) ? item.title : false;
         }).join('、');
       } else if (values[0]?.label !== undefined) {
         content = values.map((item: AnyObject) => item.label).join('、');
